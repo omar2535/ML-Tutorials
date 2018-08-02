77bc9e8022f7e99178b8975a9aed1ff3a1fc5692
Omar <omar2535@gmail.com>
2018-08-02 11:29:24 -0700

    Analysis on O-ring challenger data set for thermal distress

--- Regression/o-ring_regressor.py
+++ Regression/o-ring_regressor.py
@@ -7,31 +7,25 @@ import pandas as pd
 
 # Importing the dataset
 #:-1 means from dataset.iloc[:, 0:exclude last column]
-dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 4].values
+dataset = pd.read_csv('o-ring-erosion-only.data')
+X = dataset.iloc[:, [0, 2,3,4]].values
+y = dataset.iloc[:, 1].values
 
-#need to label encode texts for state column
-#encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
-onehotencoder = OneHotEncoder(categorical_features=[3])
-X = onehotencoder.fit_transform(X).toarray()
+print X
+print y
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=0)
+    X, y, test_size=0.01, random_state=0)
 
-#print X
-#print y
-#fitting multiple linear regression
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 #predicting the test set results
 y_pred = regressor.predict(X_test)
-print y_test
-print y_pred
+print "actual results: ",y_test
+print "prediction results: ",y_pred
+
+print "Prediction for temperature of 30deg F:", regressor.predict([[6, 31, 50, 23]])
