2cea3744c006f72d839252f5057d534d601c55b4
Omar <omar2535@gmail.com>
2018-11-29 21:48:11 -0800

    trudding along

---
+++ DeepLearning/ann.py
@@ -0,0 +1,34 @@
+# Artificial Neural Network
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+
+labelencoder_X_country = LabelEncoder()
+X[:, 1] = labelencoder_X_country.fit_transform(X[:, 1])
+labelencoder_X_gender = LabelEncoder()
+X[:, 2] = labelencoder_X_gender.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, text_size = 0.2, random_state=42)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+
+
